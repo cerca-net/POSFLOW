4b5c38b91cbc4267097bddff4f92defb770c0ef0
cerca-net <cercanetwork@gmail.com>
2025-07-18 00:45:39 -0400

    Fix Render.com deployment script and update port

--- blockchain/start_server.py
+++ blockchain/start_server.py
@@ -10,13 +10,13 @@ import argparse
 sys.path.insert(0, os.path.dirname(os.path.abspath(__file__)))
 
 # Import after path setup
-from blockchain.run_node import main
+from run_node import main
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='POS Blockchain Node')
     parser.add_argument('--ip', default='0.0.0.0', help='IP address to bind to')
-    parser.add_argument('--node_port', type=int, default=8050, help='Node port')
-    parser.add_argument('--api_port', type=int, default=8050, help='API port')
+    parser.add_argument('--node_port', type=int, default=8051, help='Node port')
+    parser.add_argument('--api_port', type=int, default=8051, help='API port')
     parser.add_argument('--key_file', default='./keys/genesis_private_key.pem', help='Private key file')
 
     args = parser.parse_args()
